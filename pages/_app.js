2ff0c1e7034a3541b1b9fe77adac6fefd2e8a3d1
kangakiles88 <kang.akiles88@gmail.com>
2023-09-07 18:49:10 +0900

    change domain meta

--- pages/_app.js
+++ pages/_app.js
@@ -19,7 +19,7 @@ function MyApp({ Component, pageProps }) {
           name="keyword"
           content="이사청소, 대청소, 청소대행, 입주청소, 안산 청소, 인천 청소, 수원 청소, 광명 청소, 대청소, 수도권 청소, 경기 청소, 전국 청소, 서울 청소"
         />
-        <meta property="og:url" content="https://a-llclean.com" />
+        <meta property="og:url" content="https://turboclea-n.com" />
         <meta property="og:type" content="website" />
         <meta
           property="og:title"
@@ -32,12 +32,13 @@ function MyApp({ Component, pageProps }) {
         <meta property="og:image" content="/ogImage.png" />
         <meta
           name="google-site-verification"
-          content="miivtKrgbqkBQWCqre84LuUPrqs1g2sdzmyjZxVUTFM"
+          content="xQQPRPE-xzW3X661sjOPJH2u3o4YEebOA5jeU_Sz-_o"
         />
         <meta
           name="naver-site-verification"
-          content="e6b6cb1106aaa0e6cc76b8ceda7ad7c2d72adc7e"
+          content="713835fa0e8b06eb884d5e39b6f9aeb1527598d8"
         />
+
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
